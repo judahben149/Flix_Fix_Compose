b3c32f5366b92c60b7bd4e33f55a8d82b9ae473d
Judah <judahben149@gmail.com>
2023-05-12 23:37:13 +0100

    Added composables for Movie List screen and App Bar

---
+++ app/src/main/java/com/judahben149/flixfixx/screens/movieList/DiscoverMovieScreen.kt
@@ -0,0 +1,42 @@
+package com.judahben149.flixfixx.screens.movieList
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.judahben149.flixfixx.screens.common.ListContent
+
+@OptIn(ExperimentalPagingApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun DiscoverMovieScreen(
+    navController: NavHostController,
+    movieListViewModel: MovieListViewModel = hiltViewModel()
+) {
+    val fetchAllDiscoverMovies =
+        movieListViewModel.fetchDiscoverMovieList.collectAsLazyPagingItems()
+
+    val scrollBehaviour = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(state = rememberTopAppBarState())
+
+    Scaffold(
+        topBar = { MovieListTopAppBar(scrollBehaviour) },
+        content = { padding ->
+            ListContent(modifier = Modifier.fillMaxSize().padding(padding), items = fetchAllDiscoverMovies)
+        }
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DiscoverMovieScreenPreview() {
+//    DiscoverMovieScreen()
+}
---
+++ app/src/main/java/com/judahben149/flixfixx/screens/movieList/MovieListTopBar.kt
@@ -0,0 +1,31 @@
+package com.judahben149.flixfixx.screens.movieList
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieListTopAppBar(scrollBehaviour: TopAppBarScrollBehavior) {
+
+    TopAppBar(
+        title = {
+            Text(
+                text = "Discover"
+            )
+        },
+        scrollBehavior = scrollBehaviour
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun MovieListTopAppBarPreview() {
+    MovieListTopAppBar(TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState()))
+}
