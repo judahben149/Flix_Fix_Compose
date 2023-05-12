b223a212a38ad4525eef3fd34f508585a332842d
Judah <judahben149@gmail.com>
2023-05-12 23:32:19 +0100

    Added App Constants

---
+++ app/src/main/java/com/judahben149/flixfixx/Constants.kt
@@ -0,0 +1,10 @@
+package com.judahben149.flixfixx
+
+object Constants {
+
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original/"
+
+    const val ITEMS_PER_PAGE = 20
+    const val INITIAL_PAGE_NUMBER = 1
+}
