0d8ca4337bcbce9b99647719b5b646b74d0ec52a
Judah <judahben149@gmail.com>
2023-05-13 10:24:17 +0100

    Added loading state views for refresh and append states

--- app/src/main/java/com/judahben149/flixfixx/screens/common/ListContent.kt
+++ app/src/main/java/com/judahben149/flixfixx/screens/common/ListContent.kt
@@ -3,6 +3,7 @@ package com.judahben149.flixfixx.screens.common
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +12,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
 import coil.annotation.ExperimentalCoilApi
@@ -25,6 +29,7 @@ import com.judahben149.flixfixx.Constants
 import com.judahben149.flixfixx.R
 import com.judahben149.flixfixx.domain.model.MovieList
 import com.judahben149.flixfixx.ui.theme.Typography
+import com.judahben149.flixfixx.utils.Extensions.parseFriendlyDate
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
@@ -45,6 +50,38 @@ fun ListContent(modifier: Modifier, items: LazyPagingItems<MovieList>) {
         ) { movieItem ->
             movieItem?.let { MovieListItem(movieListItem = movieItem) }
         }
+
+        when (val state = items.loadState.refresh) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillParentMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+
+            else -> {}
+        }
+
+        when(val state = items.loadState.append) {
+            is LoadState.Loading -> {
+                item {
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+            }
+
+            else -> {}
+        }
     }
 }
 
@@ -55,7 +92,7 @@ fun MovieListItem(movieListItem: MovieList) {
     val imageUrl = Constants.IMAGE_BASE_URL + movieListItem.posterPath
 
     val image = rememberImagePainter(data = imageUrl) {
-        crossfade(durationMillis = 400)
+        crossfade(durationMillis = 250)
         error(R.drawable.ic_placeholder)
         placeholder(R.drawable.ic_placeholder)
     }
@@ -77,15 +114,24 @@ fun MovieListItem(movieListItem: MovieList) {
                 modifier = Modifier.size(60.dp, 80.dp)
             )
 
-            Text(
-                text = movieListItem.title,
-                style = Typography.titleLarge,
-                modifier = Modifier.padding(10.dp)
-            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+            ) {
+                Text(
+                    text = movieListItem.title,
+                    style = Typography.titleLarge
+                )
+
+                Text(
+                    text = movieListItem.releaseDate.parseFriendlyDate(),
+                    style = Typography.bodySmall
+                )
+            }
+
+
         }
     }
-
-
 }
 
 @OptIn(ExperimentalCoilApi::class)
@@ -103,7 +149,7 @@ fun MovieListItemPreview() {
             overview = "",
             popularity = 0.00,
             posterPath = "",
-            releaseDate = "",
+            releaseDate = "April 20, 2023",
             title = "Guardians of the Galaxy 4",
             hasVideo = false,
             voteAverage = 0.00,
