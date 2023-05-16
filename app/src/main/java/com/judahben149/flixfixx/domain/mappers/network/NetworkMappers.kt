9e24f8739c1e74f00dccbdbdcc33df2601e7e257
Judah <judahben149@gmail.com>
2023-05-16 16:37:21 +0100

    Updated domain mapper function implementation

--- app/src/main/java/com/judahben149/flixfixx/domain/mappers/network/NetworkMappers.kt
+++ app/src/main/java/com/judahben149/flixfixx/domain/mappers/network/NetworkMappers.kt
@@ -1,9 +1,17 @@
 package com.judahben149.flixfixx.domain.mappers.network
 
 import com.judahben149.flixfixx.data.remote.model.DiscoverMoviesDataDTO
+import com.judahben149.flixfixx.data.remote.model.GenreX
+import com.judahben149.flixfixx.data.remote.model.MovieDetailDTO
+import com.judahben149.flixfixx.domain.model.Genre
+import com.judahben149.flixfixx.domain.model.Movie
 import com.judahben149.flixfixx.domain.model.MovieList
 
 interface NetworkMappers {
 
-    fun movieDtoToMovieModel(dto: DiscoverMoviesDataDTO): MovieList
+    fun movieListDtoToMovieListModel(dto: DiscoverMoviesDataDTO): MovieList
+
+    fun movieDtoToMovieModel(dto: MovieDetailDTO): Movie
+
+    fun genreDtoToGenreModel(dto: GenreX): Genre
 }
--- app/src/main/java/com/judahben149/flixfixx/domain/mappers/network/NetworkMappersImpl.kt
+++ app/src/main/java/com/judahben149/flixfixx/domain/mappers/network/NetworkMappersImpl.kt
@@ -1,11 +1,15 @@
 package com.judahben149.flixfixx.domain.mappers.network
 
 import com.judahben149.flixfixx.data.remote.model.DiscoverMoviesDataDTO
+import com.judahben149.flixfixx.data.remote.model.GenreX
+import com.judahben149.flixfixx.data.remote.model.MovieDetailDTO
+import com.judahben149.flixfixx.domain.model.Genre
+import com.judahben149.flixfixx.domain.model.Movie
 import com.judahben149.flixfixx.domain.model.MovieList
 
 object NetworkMappersImpl: NetworkMappers {
 
-    override fun movieDtoToMovieModel(dto: DiscoverMoviesDataDTO): MovieList {
+    override fun movieListDtoToMovieListModel(dto: DiscoverMoviesDataDTO): MovieList {
         return MovieList(
             id = dto.id,
             isAdult = dto.adult,
@@ -23,4 +27,39 @@ object NetworkMappersImpl: NetworkMappers {
             voteCount = dto.vote_count
         )
     }
+
+    override fun movieDtoToMovieModel(dto: MovieDetailDTO): Movie {
+        return Movie(
+            adult = dto.adult,
+            backdrop_path = dto.backdrop_path ?: "",
+            budget = dto.budget.toDouble(),
+            genres = dto.genres.map { genreDtoToGenreModel(it) },
+            homepageUrl = dto.homepage ?: "",
+            id = dto.id,
+            imdb_id = dto.imdb_id ?: "",
+            original_language = dto.original_language,
+            original_title = dto.original_title,
+            overview = dto.overview ?: "",
+            popularity = dto.popularity,
+            poster_path = dto.poster_path ?: "",
+            release_date = dto.release_date,
+            revenue = dto.revenue.toDouble(),
+            runtime = dto.runtime ?: 0,
+            status = dto.status,
+            tagline = dto.tagline ?: "",
+            title = dto.title,
+            video = dto.video,
+            vote_average = dto.vote_average,
+            vote_count = dto.vote_count
+        )
+    }
+
+    override fun genreDtoToGenreModel(dto: GenreX): Genre {
+        return Genre(
+            id = dto.id,
+            name = dto.name
+        )
+    }
+
+
 }
