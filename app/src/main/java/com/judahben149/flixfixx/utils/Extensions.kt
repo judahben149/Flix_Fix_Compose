8caab777a9706db694cc3e49e451a2bc4a39c018
Judah <judahben149@gmail.com>
2023-05-17 23:32:17 +0100

    Added convenience function to approximate ratings

--- app/src/main/java/com/judahben149/flixfixx/utils/Extensions.kt
+++ app/src/main/java/com/judahben149/flixfixx/utils/Extensions.kt
@@ -1,7 +1,9 @@
 package com.judahben149.flixfixx.utils
 
 import android.icu.text.SimpleDateFormat
+import java.text.DecimalFormat
 import java.util.Locale
+import kotlin.math.abs
 
 object Extensions {
 
@@ -25,12 +27,42 @@ object Extensions {
         return if (this >= 60) {
             hours = this / 60
             minutes = this % 60
-            "${hours.toString()}h, ${minutes.toString()}m"
+            "${hours.toString()}h${minutes.toString()}m"
         }
         else {
             minutes = this
             "${minutes.toString()}m"
         }
 
+    }
+
+    fun Double.trimDecimalPlaces(decimalPlaces: Int): Double {
+        val pattern = "#.${"0".repeat(decimalPlaces)}"
+        val decimalFormat = DecimalFormat(pattern)
+        return decimalFormat.format(this).toDouble()
+    }
+
+    fun Int.shortenVoteCount(): String {
+        if (this < 999) {
+            return this.toString()
+        }
+
+        val suffix = listOf("k", "M", "B")
+        val count = abs(this).toFloat()
+        var countDivided = 0
+
+        countDivided = (count/1000).toInt()
+        if (countDivided < 1000) {
+            return countDivided.toString().plus(suffix.get(0))
+        }
+        else {
+            countDivided = countDivided/1000
+            if (countDivided < 1000) {
+                return countDivided.toString().plus(suffix.get(1))
+            }
+            else {
+                return countDivided.toString().plus(suffix.get(2))
+            }
+        }
     }
 }
